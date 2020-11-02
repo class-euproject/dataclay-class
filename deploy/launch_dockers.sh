0e5766e14b718c3aa623cd6344af99ba6182de9c
esabate <eudald.sabate@est.fib.upc.edu>
2020-11-02 08:29:47 +0100

    Updated the dataclay version in the deploy part

--- deploy/launch_dockers.sh
+++ deploy/launch_dockers.sh
@@ -10,10 +10,10 @@ docker-compose -f dataclay/docker-compose.yml pull
 docker-compose -f dataclay/docker-compose.yml up -d
 
 echo "** Wait for dataClay to be alive **"
-docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles --network dataclay_default bscdataclay/client:2.4.dev WaitForDataClayToBeAlive 10 5
+docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles --network dataclay_default bscdataclay/client:2.5.dev WaitForDataClayToBeAlive 10 5
 
 echo "** Getting stubs from dataClay **"
-docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles -v $PWD/stubs:/stubs --network dataclay_default bscdataclay/client:2.4.dev GetStubs CityUser p4ssw0rd CityNS /stubs
+docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles -v $PWD/stubs:/stubs --network dataclay_default bscdataclay/client:2.5.dev GetStubs CityUser p4ssw0rd CityNS /stubs
 
 cd app
 python3.7 create_dkb.py
