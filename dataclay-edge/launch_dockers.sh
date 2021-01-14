6e59e3fb51588ad6fa33a9bb72ea5639a1e0a6e6
esabate <eudald.sabate@est.fib.upc.edu>
2021-01-14 17:04:43 +0100

    Adding edge part for dataclay deployment

---
+++ dataclay-edge/launch_dockers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Killing and removing dockers"
+docker-compose kill && docker-compose down -v 
+
+echo "Launching dockers"
+docker-compose up -d
+
+# check if dockers already up and registered
+#res=$(docker-compose logs 2>&1 | grep " ===> The ContractID for the registered classes is:")
+res=$(docker-compose logs 2>&1 | grep "+ echo READY")
+while [ -z "$res" ];
+do
+        echo "Dataclay initializer not ready yet. Waiting for it to finish..."
+        sleep 3
+        res=$(docker-compose logs 2>&1 | grep "+ echo READY")
+done
+echo "Dataclay initializer registered model, stubs can be retrieved."
+
+# Retrieving stubs
+./GetStubs.sh
