90f33c895a1634a957df718d9b2cabc43a438ddf
eudaldsabate <eudald.sabate@est.fib.upc.edu>
2021-04-29 15:59:44 +0200

    retrieve_objects.py just removes objects older than the specified minutes

--- dataclay-cloud/retrieve-objects/retrieve_objects.py
+++ dataclay-cloud/retrieve-objects/retrieve_objects.py
@@ -1,8 +1,6 @@
 from dataclay.api import init, finish
-from dataclay import getRuntime 
-from dataclay.api import get_backend_id_by_name
 import time
-import uuid
+import datetime
 
 # Init dataClay session
 init()
@@ -16,14 +14,15 @@ def main():
         KBob = DKB()
         KBob.make_persistent(alias="DKB")
 
+    minutes_to_sleep = 15
     while True:
         print("Checking Events Snapshot received...")
         kb = KBob.kb
         last_timestamp = 0
         if len(kb) > 0:
             last_timestamp = next(reversed(sorted(list(kb.keys()))))
-        print(f"Number of snapshots {len(list(kb.keys()))} and number of objects {len(list(KBob.objects.keys()))} and last timestamp "
-              f"{last_timestamp}")
+        print(f"Number of snapshots {len(list(kb.keys()))} and number of objects {len(list(KBob.objects.keys()))} and "
+              f"last timestamp {last_timestamp}")
         """
         for timestamp in list(kb.keys()):
             ts = timestamp
@@ -35,9 +34,15 @@ def main():
                 print(obj)
             print("#################")
         """
-        time.sleep(5)
+        time.sleep(minutes_to_sleep * 60)
         if len(kb) > 0:
             KBob.remove_old_snapshots_and_objects(last_timestamp, False)
+            # TODO: cannot remove based on .now()-10 minutes as the timestamp is set in the edge timing. Sleep of 15
+            #  minutes solves it
+            # timestamp_to_remove = int(
+            #     (datetime.datetime.now() - datetime.timedelta(minutes=minutes_to_sleep)).timestamp() * 1000)
+            # print(f"Removing objects older than: {timestamp_to_remove}")
+            # KBob.remove_old_snapshots_and_objects(timestamp_to_remove, False)
 
 
 if __name__ == "__main__":
