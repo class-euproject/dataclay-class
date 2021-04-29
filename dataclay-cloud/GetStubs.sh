2135b7fd0637cc177cf3f314953d582b631b9ce7
eudaldsabate <eudald.sabate@est.fib.upc.edu>
2021-04-29 15:08:52 +0200

    Updated to new dataclay version as there was a mismatch with the python version

--- dataclay-cloud/GetStubs.sh
+++ dataclay-cloud/GetStubs.sh
@@ -4,5 +4,5 @@ sudo rm -rf $PWD/stubs
 
 docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles \
            -v $PWD/stubs:/stubs --network host \
-           bscdataclay/client:dev20210422-alpine GetStubs CityUser p4ssw0rd CityNS /stubs
+           bscdataclay/client:dev20210428-alpine GetStubs CityUser p4ssw0rd CityNS /stubs
 
--- dataclay-edge/GetStubs.sh
+++ dataclay-edge/GetStubs.sh
@@ -4,10 +4,10 @@ rm -rf /compss/stubs
 
 docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles \
 	   -v /compss/stubs:/stubs --network host \
-	   bscdataclay/client:dev20210422-alpine GetStubs CityUser p4ssw0rd CityNS /stubs
+	   bscdataclay/client:dev20210428-alpine GetStubs CityUser p4ssw0rd CityNS /stubs
 
 
 rm /compss/dataclay.jar
-ID=$(docker create bscdataclay/logicmodule:dev20210422-alpine)
+ID=$(docker create bscdataclay/logicmodule:dev20210428-alpine)
 docker cp $ID:/home/dataclayusr/dataclay/dataclay.jar /compss
 docker rm $ID
