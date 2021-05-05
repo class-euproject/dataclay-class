f2528fa03656ab79dfef63896ccddddff14b5935
eudaldsabate <eudald.sabate@est.fib.upc.edu>
2021-05-05 15:33:46 +0200

    retrieve-objects docker changed to network mode host due to Modena cloud firewall rule. Updated GetStubs.sh to copy stubs folder with current user and group permissions and not root.

--- dataclay-cloud/GetStubs.sh
+++ dataclay-cloud/GetStubs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-sudo rm -rf $PWD/stubs
+rm -rf $PWD/stubs
 
-docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles \
-           -v $PWD/stubs:/stubs --network host \
-           bscdataclay/client:dev20210428-alpine GetStubs CityUser p4ssw0rd CityNS /stubs
+docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles       \
+           -v $PWD/stubs:/stubs --network host -e HOST_USER_ID=$(id -u)    \
+           -e HOST_GROUP_ID=$(id -g) bscdataclay/client:dev20210428-alpine \
+	   GetStubs CityUser p4ssw0rd CityNS /stubs
 
--- dataclay-edge/GetStubs.sh
+++ dataclay-edge/GetStubs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo rm -rf /compss/stubs 
+rm -rf /compss/stubs 
 
-docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles \
-	   -v /compss/stubs:/stubs --network host \
-	   bscdataclay/client:dev20210428-alpine GetStubs CityUser p4ssw0rd CityNS /stubs
-
+docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles       \
+           -v /compss/stubs:/stubs --network host -e HOST_USER_ID=$(id -u) \
+           -e HOST_GROUP_ID=$(id -g) bscdataclay/client:dev20210428-alpine \
+           GetStubs CityUser p4ssw0rd CityNS /stubs
 
 rm /compss/dataclay.jar
 ID=$(docker create bscdataclay/logicmodule:dev20210428-alpine)
