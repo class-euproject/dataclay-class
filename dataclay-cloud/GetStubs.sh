3e0554ff85f9e0fb31a6fe8cd2f3cd68e464d60a
eudaldsabate <eudald.sabate@est.fib.upc.edu>
2021-06-15 15:37:35 +0200

    Updated dataclay images to use the try-catch of objects in the cloud

--- dataclay-cloud/GetStubs.sh
+++ dataclay-cloud/GetStubs.sh
@@ -4,6 +4,6 @@ rm -rf $PWD/stubs
 
 docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles       \
            -v $PWD/stubs:/stubs --network host -e HOST_USER_ID=$(id -u)    \
-           -e HOST_GROUP_ID=$(id -g) bscdataclay/client:dev20210428-alpine \
+           -e HOST_GROUP_ID=$(id -g) bscdataclay/client:dev20210603-alpine \
 	   GetStubs CityUser p4ssw0rd CityNS /stubs
 
--- dataclay-edge/GetStubs.sh
+++ dataclay-edge/GetStubs.sh
@@ -4,10 +4,10 @@ rm -rf /compss/stubs
 
 docker run --rm -v $PWD/cfgfiles:/home/dataclayusr/dataclay/cfgfiles       \
            -v /compss/stubs:/stubs --network host -e HOST_USER_ID=$(id -u) \
-           -e HOST_GROUP_ID=$(id -g) bscdataclay/client:dev20210428-alpine \
+           -e HOST_GROUP_ID=$(id -g) bscdataclay/client:dev20210603-alpine \
            GetStubs CityUser p4ssw0rd CityNS /stubs
 
 rm /compss/dataclay.jar
-ID=$(docker create bscdataclay/logicmodule:dev20210428-alpine)
+ID=$(docker create bscdataclay/logicmodule:dev20210603-alpine)
 docker cp $ID:/home/dataclayusr/dataclay/dataclay.jar /compss
 docker rm $ID
