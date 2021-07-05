4b6574f285ae917ed46bc8f1ddf1255ebc966a04
eudaldsabate <eudald.sabate@est.fib.upc.edu>
2021-07-05 13:23:29 +0200

    Updated image name for removal

--- dataclay-edge/stop_all_containers.sh
+++ dataclay-edge/stop_all_containers.sh
@@ -10,5 +10,5 @@ declare -a fog_nodes=('192.168.12.1' '192.168.12.2' '192.168.12.3' '192.168.12.4
 for ip in ${fog_nodes[@]}
 do
 	#ssh ${main_server_user}@${main_server_ip} "ssh ${fog_node_user}@${ip} 'docker container ls -a | awk "'"{print \$NF}"'" | grep tracking_partial | xargs -r docker container stop'"
-	ssh ${main_server_user}@${main_server_ip} "ssh ${fog_node_user}@${ip} 'docker container ls -a | awk "'"{print \$NF}"'" | grep tracking_partial | xargs -r docker container rm -f'"
+	ssh ${main_server_user}@${main_server_ip} "ssh ${fog_node_user}@${ip} 'docker container ls -a | awk "'"{print \$NF}"'" | grep class-object-tracking | xargs -r docker container rm -f'"
 done
